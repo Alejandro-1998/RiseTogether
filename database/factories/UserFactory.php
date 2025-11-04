b0fcfcd5253ff201af6c761caa81fc33418432eb
Alejandro-Medac <alcauq@gmail.com>
2025-11-04 18:13:51 +0100

    correccion UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,25 +4,36 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     public function definition(): array
     {
+        // === Generar DNI español (8 dígitos + letra) ===
+        $numero = rand(10000000, 99999999);
+        $letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+        $letra = $letras[$numero % 23];
+        $dni = "{$numero}{$letra}";
+
+        // === Generar IBAN simple (no real, solo para test) ===
+        $iban = 'ES' . str_pad(strval(rand(0, 999999999999999999)), 18, '0', STR_PAD_LEFT);
+
         return [
-            'dni' => $this->faker->optional()->unique()->numerify('#########'),
-            'nombreUsuario' => $this->faker->unique()->userName(),
-            'nombreCompleto' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'dni' => fake()->boolean(80) ? $dni : null, // 80% de probabilidad de tener DNI
+            'nombreUsuario' => fake()->unique()->userName(),
+            'nombreCompleto' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'), // contraseña por defecto
-            'fechaNacimiento' => $this->faker->optional()->date(),
-            'direccion' => $this->faker->optional()->address(),
-            'numeroCuenta' => $this->faker->optional()->unique()->numerify('ES####################'),
+            'password' => bcrypt('password'),
+            'fechaNacimiento' => fake()->date(),
+            'direccion' => fake()->optional()->address(),
+            'numeroCuenta' => fake()->boolean(70) ? $iban : null, // 70% de probabilidad de tener cuenta
             'remember_token' => Str::random(10),
         ];
     }
